698ad88a1112e5390e3221e43bb646d3540cf83e
K2LU <rigansust@gmail.com>
2025-11-25 12:44:48 +0600

    feat: support for dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import Express from "express";
 const app = Express()
 
@@ -11,9 +13,21 @@ import cors from "cors"
 import cookieParser from "cookie-parser";
 // middlewares
 // parse JSON bodies (only once)
+
+
 app.use(Express.json())
-app.use(cors())
+// app.use(cors())
 app.use(cookieParser())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(
+  cors({
+    origin: process.env.FRONTEND_BASE_URL,
+    credentials: true // Allow cookies to be sent/received
+  })
+);
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
@@ -21,6 +35,6 @@ app.use("/api/post", postRoutes)
 app.use("/api/vote", voteRoutes)
 app.use("/api/comment", commentRoutes)
 
-app.listen(3000, () => {
+app.listen(8000, () => {
     console.log("app listening");
 })
